a40c94aa1ac04e001e37a055ec78830c4fb05f1f
AlexxanderP <AlexxanderP@users.noreply.github.com>
2019-12-06 00:38:54 -0800

    working through Blockchain Day 1 Homework

--- client_mining_p/miner.py
+++ client_mining_p/miner.py
@@ -4,6 +4,8 @@ import requests
 import sys
 import json
 
+DIFFICULTY= 3
+
 
 def proof_of_work(block):
     """
@@ -13,7 +15,12 @@ def proof_of_work(block):
     in an effort to find a number that is a valid proof
     :return: A valid proof for the provided block
     """
-    pass
+    block_string = json.dumps(self.last_block, sort_keys=True)
+    proof = 0
+    while self.valid_proof(block_string, proof) is False:
+        proof += 1
+
+    return proof
 
 
 def valid_proof(block_string, proof):
@@ -27,7 +34,9 @@ def valid_proof(block_string, proof):
     correct number of leading zeroes.
     :return: True if the resulting hash is a valid proof, False otherwise
     """
-    pass
+    guess = f'{block_string}{proof}'.encode()
+    guess_hash = hashlib.sha256(guess).hexdigest()
+    return guess_hash[:DIFFICULTY] == "0" * DIFFICULTY
 
 
 if __name__ == '__main__':
@@ -45,6 +54,7 @@ if __name__ == '__main__':
 
     # Run forever until interrupted
     while True:
+        print("Starting to look for Proof") """<---- """
         r = requests.get(url=node + "/last_block")
         # Handle non-json response
         try:
@@ -55,8 +65,10 @@ if __name__ == '__main__':
             print(r)
             break
 
-        # TODO: Get the block from `data` and use it to look for a new proof
-        # new_proof = ???
+        # TODO: Get the block from `data` and use it to look for a new proof ####SECOND
+        last_block = data['last_block']
+        new_proof = proof_of_work(last_block)
+        print(f"Proof found: {new_proof}") """<---- """
 
         # When found, POST it to the server {"proof": new_proof, "id": id}
         post_data = {"proof": new_proof, "id": id}
